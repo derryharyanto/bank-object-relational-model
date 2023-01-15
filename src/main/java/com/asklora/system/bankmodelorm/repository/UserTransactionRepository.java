07419daaa00db22820ecf4cdbcfa14ed0824626d
derryharyanto <122698073+derryharyanto@users.noreply.github.com>
2023-01-15 23:27:19 +0700

    Delete src/main directory

--- src/main/java/com/asklora/system/bankmodelorm/repository/UserTransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.asklora.system.bankmodelorm.repository;
-
-import com.asklora.system.bankmodelorm.entity.UserTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTransactionRepository extends JpaRepository<UserTransaction,String> {
-
-}
